03ac46572d5c352d7a8777ffaf8e009641153cc8
Quentin Beraud <qbe.pro@yahoo.com>
2024-02-14 16:09:52 +0100

    First commit

---
+++ src/main/java/com/medilaboSolutions/diabeteDetect/DiabeteDetectApplication.java
@@ -0,0 +1,13 @@
+package com.medilaboSolutions.diabeteDetect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabeteDetectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabeteDetectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medilaboSolutions/diabeteDetect/DiabeteDetectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilaboSolutions.diabeteDetect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabeteDetectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
