406c44868ac1bb054e27ab08fbfd1e6629c17d84
Quentin Beraud <qbe.pro@yahoo.com>
2024-03-11 16:42:15 +0100

    Add diabetes assessment Microservice

---
+++ diabetesAssessment/src/main/java/com/mediaSolutions/diabetesAssessment/DiabetesAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.mediaSolutions.diabetesAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabetesAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabetesAssessmentApplication.class, args);
+	}
+
+}
---
+++ diabetesAssessment/src/test/java/com/mediaSolutions/diabetesAssessment/DiabetesAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediaSolutions.diabetesAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabetesAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
